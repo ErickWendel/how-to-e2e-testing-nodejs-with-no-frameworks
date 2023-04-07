5bf1ff6c3f2ab47c969b8a2aa456b171b4181a90
Erick Wendel <erick.workspace@gmail.com>
2023-04-07 19:16:01 -0300

    add examples in class
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- recorded/app/api.test.js
+++ recorded/app/api.test.js
@@ -1,15 +1,13 @@
 import { describe, before, after, it } from 'node:test'
-import { deepStrictEqual, ok } from 'node:assert'
-let _globalToken = ''
-let BASE_URL = 'http://localhost:3000'
-
+import { deepStrictEqual, ok, strictEqual } from 'node:assert'
+const BASE_URL = 'http://localhost:3000'
 describe('API Workflow', () => {
   let _server = {}
+  let _globalToken = ''
   before(async () => {
     _server = (await import('./api.js')).app
     await new Promise(resolve => _server.once('listening', resolve))
   })
-
   after(done => _server.close(done))
 
   it('should receive not authorized given wrong user and password', async () => {
@@ -19,14 +17,12 @@ describe('API Workflow', () => {
     }
     const request = await fetch(`${BASE_URL}/login`, {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     })
-
-    deepStrictEqual(request.status, 400)
+    strictEqual(request.status, 401)
     const response = await request.json()
     deepStrictEqual(response, { error: 'user invalid!' })
   })
-
   it('should login successfuly given user and password', async () => {
     const data = {
       user: 'erickwendel',
@@ -34,35 +30,38 @@ describe('API Workflow', () => {
     }
     const request = await fetch(`${BASE_URL}/login`, {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     })
-
-    deepStrictEqual(request.status, 200)
+    strictEqual(request.status, 200)
     const response = await request.json()
-    ok(response.token, "token should be present")
+    ok(response.token, 'token should be present')
     _globalToken = response.token
   })
 
   it('should not be allowed to access private data without a token', async () => {
-    const headers = {
-      authorization: ''
-    }
-    const request = await fetch(`${BASE_URL}`, {
-      headers
+ 
+    const request = await fetch(`${BASE_URL}/`, {
+      method: 'GET',
+      headers: {
+        authorization: ''
+      }
     })
-    deepStrictEqual(request.status, 400)
+
+    strictEqual(request.status, 400)
     const response = await request.json()
-    deepStrictEqual(response, { result: 'invalid token!' })
+    deepStrictEqual(response, { error: 'invalid token!' })
   })
 
-  it('should be allowed to access private data given a valid token', async () => {
-    const headers = {
-      authorization: _globalToken
-    }
-    const request = await fetch(`${BASE_URL}`, {
-      headers
+  it('should be allowed to access private data with a valid token', async () => {
+ 
+    const request = await fetch(`${BASE_URL}/`, {
+      method: 'GET',
+      headers: {
+        authorization: _globalToken
+      }
     })
-    deepStrictEqual(request.status, 200)
+
+    strictEqual(request.status, 200)
     const response = await request.json()
     deepStrictEqual(response, { result: 'Hey welcome!' })
   })
